f8e3c2da08e0c157dcc9470c5b8159840da29dee
Alec Hajinoff <hajinoff@gmail.com>
2026-02-18 06:19:42 +0000

    18/2/26, 6:19

--- React/src/TimelineFilter.js
+++ React/src/TimelineFilter.js
@@ -1,26 +1,10 @@
 import React from "react";
 import "./TimelineFilter.css";
 
-const CATEGORY_OPTIONS = [
-  "Core Role / Job Performance",
-  "Leadership / Ownership",
-  "Cross-Team Collaboration",
-  "Problem Solving / Innovation",
-  "Client / Stakeholder Impact",
-  "Process Improvement / Efficiency",
-  "Mentoring / Knowledge Sharing",
-];
-
 const TimelineFilter = ({ filters, setFilters, onClear }) => {
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFilters((prev) => ({ ...prev, [name]: value }));
-  };
-
-  const handleCategoriesChange = (e) => {
-    const options = Array.from(e.target.options);
-    const selected = options.filter((o) => o.selected).map((o) => o.value);
-    setFilters((prev) => ({ ...prev, selectedCategories: selected }));
   };
 
   return (
@@ -47,25 +31,6 @@ const TimelineFilter = ({ filters, setFilters, onClear }) => {
               value={filters.endDate}
               onChange={handleChange}
             />
-          </div>
-
-          <div className="col-md-6">
-            <label className="form-label small">
-              Categories (Select multiple)
-            </label>
-            <select
-              multiple
-              className="form-select form-select-sm"
-              value={filters.selectedCategories}
-              onChange={handleCategoriesChange}
-              style={{ height: "80px" }}
-            >
-              {CATEGORY_OPTIONS.map((cat) => (
-                <option key={cat} value={cat}>
-                  {cat}
-                </option>
-              ))}
-            </select>
           </div>
         </div>
 
