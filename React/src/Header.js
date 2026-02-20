fc387a348a86cf1fa96c3857eb6e6923ef0fb2ef
Alec Hajinoff <hajinoff@gmail.com>
2026-02-20 07:01:11 +0000

    20/2/26, 7:01

--- React/src/Header.js
+++ React/src/Header.js
@@ -1,4 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import blue from "./ContributionLineLogoColoured.png";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="container">
+      <div className="row">
+        <Link to="/">
+          <img 
+            id="logo" 
+            src={blue} 
+            alt="A company logo" 
+            title="A company logo"
+          />
+        </Link>
+      </div>
+      <div className="row-auto">
+        <br />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
+
+/*
+import React from "react";
 import blue from "./ContributionLineLogoColoured.png";
 
 import "./Header.css";
@@ -17,3 +45,4 @@ function Header() {
 }
 
 export default Header;
+*/
